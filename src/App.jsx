f41821a0eadaf662a5884691f491e9f4b58542e7
vanshmathur2003 <vanshmathur26@gmail.com>
2024-02-25 02:09:50 +0530

    card made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar"
 import Section1 from "./Components/Section1"
+import  Card  from "./Components/Card"
 function App() {
 
   return (
     <>
     <Navbar/>
     <Section1/>
+    <Card/>
     </>
   )
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,44 @@
+import "../Components/Card.scss"
+import pcimg from "../assets/icons/download 1.png";
+import bluedownarrow from "../assets/icons/IMAGE (6).png"
+import fivestar from "../assets/icons/IMAGE (5).png";
+import fourpointfivestar from "../assets/icons/IMAGE (3).png";
+import fourstar from "../assets/icons/IMAGE (4).png";
+
+const Card = () => {
+    return (
+        <>
+            <div className="cardMain">
+                <div className="cardBox">
+                    <div className="cardBoxSection1">
+                        <img src={pcimg} />
+                        <div>Builder 1</div>
+                    </div>
+                    <div className="cardBoxSection2">
+                        <div className="cardBoxSection2text one-1"> <b> WixPro 72-Inch Responsive Website Builder- </b>Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)</div>
+                        <div className="cardBoxSection2text 2"><b>Main highlights</b><br /><br />
+                        What You Get: Receive the WixPro website builder suite, access to premium design templates, an extensive library of
+                            wiagers ana apps, and detallea step-by-step guldes</div>
+                        <div className="cardBoxSection2textblue">
+                        <div className="cardBoxSection2textbluetxt">Show more </div>
+                        <img src={bluedownarrow} alt="" />
+                        </div>
+                        <div />
+                    </div>
+                    <div className="cardBoxSection3">
+                        <div className="ratingcardBoxSection3">
+                            <div className="ratingcardBoxSection3-1" >9.8</div>
+                            <div className="ratingcardBoxSection3-2" >Exceptional</div>
+                            <div className="ratingcardBoxSection3-3" ><img src={fivestar} alt="" /></div>
+                        </div>
+                        <div>
+                        <div><button className="btn">View</button></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
